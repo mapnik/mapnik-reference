5cc859cc66b59498c307796551006132d89755f6
Dane Springmeyer <dane@dbsgeo.com>
2011-04-25 18:47:39 -0700

    make sure every property has a type defined

--- test.py
+++ test.py
@@ -13,4 +13,11 @@ reference = json.load(open('reference.json','r'))
 # TODO - validate stuff
 assert reference
 
+for sym in reference['symbolizers'].items():
+    assert sym[1]
+    for i in sym[1].items():
+        #import pdb;pdb.set_trace()
+        assert 'type' in i[1].keys(), 'type not in %s' % i[0]
+        #assert 'doc' in i[1].keys(), 'type not in %s' % i[0]
+
 print '... oh yeah, tests passed'
