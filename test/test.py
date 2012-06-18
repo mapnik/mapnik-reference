34a934a200196d3e0890b137898e8ccce75edbb4
Tom MacWright <tom@macwright.org>
2012-06-18 13:23:05 -0400

    Improve testing script, redact properties not supported by 2.0.1, add doc strings.

--- test/test.py
+++ test/test.py
@@ -7,15 +7,16 @@ except ImportError:
     # >= python 2.6
     import json
 
+versions = ['2.0.1', '2.1.0', 'latest']
 
-reference = json.load(open('latest/reference.json','r'))
-
-# TODO - validate stuff
-assert reference
-
-for sym in reference['symbolizers'].items():
-    assert sym[1]
-    for i in sym[1].items():
-        assert 'type' in i[1].keys(), 'type not in %s' % i[0]
+for v in versions:
+    print '-- testing %s/reference.json' % v
+    reference = json.load(open('%s/reference.json' % v, 'r'))
+    assert reference
+    for sym in reference['symbolizers'].items():
+        assert sym[1]
+        for i in sym[1].items():
+            assert 'type' in i[1].keys(), '%s: type not in %s' % (sym[0], i[0])
+            assert 'doc' in i[1].keys(), '%s: doc string not in %s' % (sym[0], i[0])
 
 print '... oh yeah, tests passed'
