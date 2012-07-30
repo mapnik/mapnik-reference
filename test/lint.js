a5ecc9a2269e133a6af867147d7f132b33c06dfc
Dane Springmeyer <dane@dbsgeo.com>
2012-07-30 15:30:53 -0700

    touchup tests to run lint again all versions

--- test/lint.js
+++ test/lint.js
@@ -1,48 +1,58 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var reference = require("..");
 
-var reference = JSON.parse(fs.readFileSync('reference.json','utf8'));
+var lint_ref = function(reference) {
+    var symbolizers = reference.symbolizers;
 
-var symbolizers = reference.symbolizers;
-
-console.log('Missing doc properties');
-console.log('----------------------');
-var missing_doc_properties = 0;
-var cursim = '';
-for (var symbolizer in symbolizers) {
-    if (symbolizer === 'colors') continue;
-    for (var prop in symbolizers[symbolizer]) {
-        if (!symbolizers[symbolizer][prop].doc) {
-            if (symbolizer !== cursim) {
-                cursim = symbolizer;
-                console.log(symbolizer);
+    console.log('Missing doc properties');
+    console.log('----------------------');
+    var missing_doc_properties = 0;
+    var cursim = '';
+    for (var symbolizer in symbolizers) {
+        if (symbolizer === 'colors') continue;
+        for (var prop in symbolizers[symbolizer]) {
+            if (!symbolizers[symbolizer][prop].doc) {
+                if (symbolizer !== cursim) {
+                    cursim = symbolizer;
+                    console.log(symbolizer);
+                }
+                console.log('- ' + prop);
+                missing_doc_properties++;
             }
-            console.log('- ' + prop);
-            missing_doc_properties++;
         }
     }
-}
-console.log('----------------------');
-console.log(missing_doc_properties, 'missing doc properties');
-console.log('----------------------');
+    console.log('----------------------');
+    console.log(missing_doc_properties, 'missing doc properties');
+    console.log('----------------------');
 
-console.log('Missing default value');
-console.log('----------------------');
-var missing_doc_properties = 0;
-var cursim = '';
-for (var symbolizer in symbolizers) {
-    if (symbolizer === 'colors') continue;
-    for (var prop in symbolizers[symbolizer]) {
-        if (symbolizers[symbolizer][prop]['default-value'] === undefined) {
-            if (symbolizer !== cursim) {
-                cursim = symbolizer;
-                console.log(symbolizer);
+    console.log('Missing default value');
+    console.log('----------------------');
+    var missing_doc_properties = 0;
+    var cursim = '';
+    for (var symbolizer in symbolizers) {
+        if (symbolizer === 'colors') continue;
+        for (var prop in symbolizers[symbolizer]) {
+            if (symbolizers[symbolizer][prop]['default-value'] === undefined) {
+                if (symbolizer !== cursim) {
+                    cursim = symbolizer;
+                    console.log(symbolizer);
+                }
+                console.log('- ' + prop);
+                missing_doc_properties++;
             }
-            console.log('- ' + prop);
-            missing_doc_properties++;
         }
     }
+    console.log('----------------------');
+    console.log(missing_doc_properties, 'missing default-value');
 }
-console.log('----------------------');
-console.log(missing_doc_properties, 'missing default-value');
+
+console.log('\n\n---> 2.0.1');
+lint_ref(reference.version['2.0.1']);
+
+console.log('\n\n---> 2.1.0');
+lint_ref(reference.version['2.1.0']);
+
+console.log('\n\n---> latest');
+lint_ref(reference.version['latest']);
