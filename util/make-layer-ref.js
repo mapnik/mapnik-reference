16931e395f7bf3a71497cd8af216bdfbb2aedf22
Dane Springmeyer <dane@dbsgeo.com>
2013-01-03 13:10:14 -0800

    add more csv and postgis options to layers.json reference - refs #44 and mapbox/tilemill#994

--- util/make-layer-ref.js
+++ util/make-layer-ref.js
@@ -1,10 +1,34 @@
 var fs = require('fs');
 var path = require('path');
+var util = require('util');
+var assert = require('assert');
 var existsSync = require('fs').existsSync || require('path').existsSync
 
 var args = process.argv.slice(1);
 
 var cmd = path.basename(process.argv.slice(1,2));
+
+var options = {};
+
+args = args.filter(function (arg) {
+    var match;
+
+    if (match = arg.match(/^--?([a-z][0-9a-z-]*)$/i)) { arg = match[1] }
+    else { return arg }
+
+    switch (arg) {
+        case 'd':
+        case 'debug':
+            options.debug = true;
+            break;
+        default:
+            console.log("Usage: "+cmd+" <source file>");
+            console.log("Options:");
+            console.log("  -d   --debug");
+            process.exit(0);
+            break;
+    }
+});
 
 var input = args[1];
 if (input && input[0] != '/') {
@@ -30,15 +54,38 @@ if (!existsSync(input)) {
 
 var data = JSON.parse(fs.readFileSync(input));
 
+// TODO - add support for includes?
+
+function validate_prop(prop) {
+    assert.ok('type' in prop,'type not defined for '+ util.inspect(prop));
+    assert.ok('doc' in prop,'doc not defined for '+ util.inspect(prop));
+    assert.ok('default-value' in prop,'default-value not defined for '+ util.inspect(prop));
+    assert.ok('default-meaning' in prop,'default-value not defined for '+ util.inspect(prop));
+}
+
 // expand gyp-like variables to build out entire file
 Object.keys(data.datasources).forEach(function(key) {
     var ds = data.datasources[key];
-    Object.keys(ds).forEach(function(prop) {
-        var match = ds[prop].match(/<@\((.+)\)/);
-        if (match && match[1]) {
-            ds[prop] = data.variables[prop];
-        }
-    });
+    if (options.debug) console.warn('Handling '+key)
+    // handle commented sections
+    if (key[0] == '#') {
+        delete data.datasources[key];
+    } else {
+        Object.keys(ds).forEach(function(prop) {
+            if (options.debug) {
+              console.warn('  parsing "'+prop+'" ('+typeof(prop)+')');
+              console.warn('  parsing "'+ util.inspect(ds[prop])+'"')
+            }
+            var match = ds[prop].match && ds[prop].match(/<@\((.+)\)/);
+            if (match && match[1]) {
+                ds[prop] = data.variables[prop];
+            }
+            validate_prop(ds[prop]);
+        });
+    }
 });
 
-console.log(JSON.stringify(data,null,"    "));
+if (!options.debug) {
+  delete data.variables;
+  console.log(JSON.stringify(data,null,"    "));
+}
