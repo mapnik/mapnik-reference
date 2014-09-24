59dae19228742fa8162d506d77b231391488a51e
Dane Springmeyer <dane@mapbox.com>
2014-09-23 18:49:50 -0700

    port symbolizer linting to js script only

--- test/lint.js
+++ test/lint.js
@@ -7,8 +7,20 @@ var lint_ref = function(ver) {
     var ref = references.version[ver]
     var symbolizers = ref.symbolizers;
     for (var symbolizer in symbolizers) {
+        //console.log(symbolizer);
         for (var prop in symbolizers[symbolizer]) {
             var sym = symbolizers[symbolizer][prop];
+            var group_name = symbolizer;
+            if (group_name == "markers") {
+                group_name = 'marker';
+            }
+            if (group_name != 'map' &&
+                group_name != '*' &&
+                sym.css.indexOf(group_name) == -1)
+                console.log('invalid prefix for property',sym.css,ver);
+            if (!sym.type) {
+                console.log('missing type for',symbolizer+'-'+prop,ver);
+            }
             if (!sym.doc) {
                 console.log('missing doc for',symbolizer+'-'+prop,ver);
             }
