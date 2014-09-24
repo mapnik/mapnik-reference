916612da80f1a7e92a9da1d33558261c16fd20db
Dane Springmeyer <dane@mapbox.com>
2014-09-23 18:41:08 -0700

    simplify linting code

--- test/lint.js
+++ test/lint.js
@@ -6,40 +6,17 @@ var references = require("..");
 var lint_ref = function(ver) {
     var ref = references.version[ver]
     var symbolizers = ref.symbolizers;
-
-    var missing_doc_properties = 0;
-    var cursim = '';
     for (var symbolizer in symbolizers) {
-        if (symbolizer === 'colors') continue;
         for (var prop in symbolizers[symbolizer]) {
-            if (!symbolizers[symbolizer][prop].doc) {
-                if (symbolizer !== cursim) {
-                    cursim = symbolizer;
-                    console.log(symbolizer);
-                }
-                console.log('- ' + prop);
-                missing_doc_properties++;
+            var sym = symbolizers[symbolizer][prop];
+            if (!sym.doc) {
+                console.log('missing doc for',symbolizer+'-'+prop,ver);
+            }
+            if (sym['default-value'] === undefined) {
+                console.log('missing default-value for',symbolizer+'-'+prop,ver);
             }
         }
     }
-    if (missing_doc_properties > 0) console.log(missing_doc_properties, 'missing doc properties',ver);
-
-    var miss_default_value = 0;
-    var cursim = '';
-    for (var symbolizer in symbolizers) {
-        if (symbolizer === 'colors') continue;
-        for (var prop in symbolizers[symbolizer]) {
-            if (symbolizers[symbolizer][prop]['default-value'] === undefined) {
-                if (symbolizer !== cursim) {
-                    cursim = symbolizer;
-                    console.log(symbolizer);
-                }
-                console.log('- ' + prop);
-                miss_default_value++;
-            }
-        }
-    }
-    if (miss_default_value > 0) console.log(miss_default_value, 'missing default-value',ver);
 }
 
 for (var key in references.version) {
