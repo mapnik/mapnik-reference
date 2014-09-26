c065016fbe8e8dbc66b4d85064f8f5d6bb615975
Dane Springmeyer <dane@mapbox.com>
2014-09-25 17:59:56 -0700

    tests: detect versions via globbing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
 var references = require('mapnik-reference');
+var glob = require('glob');
+var path = require('path');
 
 describe('datasources', function() {
     for (var key in references.version) {
@@ -18,15 +20,27 @@ describe('datasources', function() {
 
 describe('styles', function() {
     for (var key in references.version) {
-        var spec = references.version[key];
-        it('show report correct version for '+key, function() {
-            assert.equal(spec.version,key);
-        });
+        (function(key) {
+            it('show report correct version for '+key, function() {
+                assert.equal(references.version[key].version,key);
+            });
+        })(key)
     }
 
     it('show reveal new property in Mapnik 2.3.0', function() {
         assert.ok(references.version['2.3.0'].symbolizers.markers['geometry-transform']);
         assert.ok(!references.version['2.0.0'].symbolizers.markers['geometry-transform']);
     });
-
 });
+
+
+describe('versions', function() {
+    it('show report all versions', function() {
+        var versions = glob.sync("./*.*.*");
+        var versions2 = Object.keys(references.version);
+        versions = versions.map(function(e) { return path.basename(e) })
+        assert.deepEqual(versions,versions2);
+    });
+});
+
+
