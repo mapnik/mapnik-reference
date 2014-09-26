edec8c1df85e5eb148236e0d6aec690533391a85
Dane Springmeyer <dane@mapbox.com>
2014-09-25 17:59:37 -0700

    python test: detect versions via glob

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import glob
+import os
 try:
     # <= python 2.5
     import simplejson as json
@@ -7,9 +9,10 @@ except ImportError:
     # >= python 2.6
     import json
 
-versions = ['2.0.0','2.0.1', '2.0.2', '2.1.0', '2.1.1', '2.3.0', 'latest']
+versions = glob.glob('./*.*.*')
 
 for v in versions:
+    ver = os.path.basename(v)
     reference = json.load(open('%s/reference.json' % v, 'r'))
     assert reference
-    assert reference['version'] == v,"%s not eq to %s" % (reference['version'],v)
+    assert reference['version'] == ver,"%s not eq to %s" % (reference['version'],ver)
