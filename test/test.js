809268a822daabd767610d1e65aa78efa4de75f8
Dane Springmeyer <dane@mapbox.com>
2014-09-25 17:36:48 -0700

    few more tests

--- test/test.js
+++ test/test.js
@@ -15,3 +15,18 @@ describe('datasources', function() {
         }
     }
 });
+
+describe('styles', function() {
+    for (var key in references.version) {
+        var spec = references.version[key];
+        it('show report correct version for '+key, function() {
+            assert.equal(spec.version,key);
+        });
+    }
+
+    it('show reveal new property in Mapnik 2.3.0', function() {
+        assert.ok(references.version['2.3.0'].symbolizers.markers['geometry-transform']);
+        assert.ok(!references.version['2.0.0'].symbolizers.markers['geometry-transform']);
+    });
+
+});
