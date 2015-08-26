3a35d70f8a33f69ab76a35a370a4948b47b7c22f
artemp <artem@mapnik.org>
2015-08-26 16:20:17 +0200

    fix version num

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ var versions = [
  '2.2.0',
  '2.3.0',
  '3.0.0',
- '3.0.3'
+ '3.0.3',
+ '3.0.4'
 ];
 
 module.exports.versions = versions;
 
 module.exports.load = function(version) {
     if (versions.indexOf(version) <= -1) {
-        throw new Error("Unknown mapnik-reference version: '" + version + "'");
+	throw new Error("Unknown mapnik-reference version: '" + version + "'");
     }
     var ref = require(path.join(__dirname, version, 'reference.json'));
     var ds_path = path.join(__dirname, version, 'datasources.json');
     if (existsSync(ds_path)) {
-        ref.datasources = require(ds_path).datasources;
+	ref.datasources = require(ds_path).datasources;
     }
     return ref;
 }
